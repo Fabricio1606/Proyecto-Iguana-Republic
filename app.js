a2c16bc6a1c7c4d68897cec33c8aaaf71730706e
eagl2002 <41276576+eagl2002@users.noreply.github.com>
2024-02-02 00:30:09 +0000

    Solucionando problemas

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const authController = require('./controllers/authController');
 const authRoutes = require('./Routes/authRoutes');
 const connection = require('./models/db');
 const path = require('path');
-
 const app = express();
 const port = 3000;
 
@@ -17,16 +16,16 @@ connection.connect((err) => {
 
 // Configuración de Express
 app.use(session({
-  secret: 'miSecreto',
-  resave: true,
-  saveUninitialized: true,
+secret: 'miSecreto',
+resave: true,
+saveUninitialized: true,
 }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 // Configuración de middleware y rutas adicionales
-app.use(express.static(path.resolve('./public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Configuración de rutas
 const mainController = new MainController();
--- controllers/authController.js
+++ controllers/authController.js
@@ -4,48 +4,12 @@ const bcrypt = require('bcrypt');
 
 class AuthController {
     constructor() {
-    
+
     }
-    getRegister(req, res) {
-        const { nombre, correo, nacionalidad, usuario, password } = req.body;
-        res.render('register');
-
-        // Verificar si el usuario ya existe
-        connection.query('SELECT * FROM Client WHERE userClient = ?', [usuario], (error, results) => {
-            if (error) {
-                return res.status(500).send('Error en el servidor.');
-            }
-
-            if (results.length > 0) {
-                // El nombre de usuario ya está en uso
-                return res.render('register', { message: 'El nombre de usuario ya está en uso.' });
-            }
-
-            // Hash de la contraseña antes de almacenarla
-            bcrypt.hash(password, 10, (err, hash) => {
-                if (err) {
-                    return res.status(500).send('Error en el servidor.');
-                }
-
-                // Insertar el cliente en la base de datos
-                connection.query(
-                    'INSERT INTO Client (nameClient, mailClient, nationClient, userClient, passClient_hash) VALUES (?, ?, ?, ?, ?)',
-                    [nombre, correo, nacionalidad, usuario, hash],
-                    (error) => {
-                        if (error) {
-                            return res.status(500).send('Error en el servidor.');
-                        }
-
-                        return res.render('register', { message: 'Registro exitoso. Ahora puedes iniciar sesión.' });
-                    }
-                );
-            });
-        });
-    };
 
     getLogin(req, res) {
         const { username, password } = req.body;
-        res.render('login');
+
         // Obtener el cliente de la base de datos
         connection.query('SELECT * FROM Client WHERE userClient = ?', [username], (error, results) => {
             if (error) {
@@ -75,8 +39,44 @@ class AuthController {
                 return res.render('login', { message: 'Cliente no encontrado.' });
             }
         });
-    };
-}
+    }
 
+    getRegister(req, res) {
+        const { nombre, correo, nacionalidad, usuario, password } = req.body;
+
+        // Verificar si el usuario ya existe
+        connection.query('SELECT * FROM Client WHERE userClient = ?', [usuario], (error, results) => {
+            if (error) {
+                return res.status(500).send('Error en el servidor.');
+            }
+
+            if (results.length > 0) {
+                // El nombre de usuario ya está en uso
+                return res.render('register', { message: 'El nombre de usuario ya está en uso.' });
+            }
+
+            // Hash de la contraseña antes de almacenarla
+            bcrypt.hash(password, 10, (err, hash) => {
+                if (err) {
+                    return res.status(500).send('Error en el servidor.');
+                }
+
+                // Insertar el cliente en la base de datos
+                connection.query(
+                    'INSERT INTO Client (nameClient, mailClient, nationClient, userClient, passClient_hash) VALUES (?, ?, ?, ?, ?)',
+                    [nombre, correo, nacionalidad, usuario, hash],
+                    (error) => {
+                        if (error) {
+                            return res.status(500).send('Error en el servidor.');
+                        }
+
+                        // Registro exitoso, redirigir al cliente a la página de inicio de sesión
+                        return res.redirect('/login');
+                    }
+                );
+            });
+        });
+    }
+}
 
 module.exports = new AuthController();
