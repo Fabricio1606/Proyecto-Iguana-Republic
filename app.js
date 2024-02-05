f9144174c2912ed209861ef0454cb8226f97084c
eagl2002 <41276576+eagl2002@users.noreply.github.com>
2024-02-05 03:42:03 +0000

    a

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
 const express = require('express');
-<<<<<<< HEAD
 const MainController = require('./controllers/mainController');
 const connection = require('./models/db');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
-=======
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const MainController = require('./controllers/mainController');
-const AuthController = require('./controllers/authController');
-const connection = require('./models/db');
-const path = require('path');
-const app = express();
-const port = 3000;
->>>>>>> 8cee5fb363a9b19f69ef26c7fd9fe8aa2a6df69f
+const port = 8050;
+
+
 
 connection.connect((err) => {
 
 });
 
-<<<<<<< HEAD
 app.use(express.json());
-=======
->>>>>>> 8cee5fb363a9b19f69ef26c7fd9fe8aa2a6df69f
 
 // Configuración de Express
 app.use(session({
@@ -36,7 +26,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 // Configuración de middleware y rutas adicionales
-<<<<<<< HEAD
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -50,26 +39,23 @@ app.use((req, res, next) => {
   next();
 });
 
-=======
-app.use(express.static(path.join(__dirname, 'public')));
->>>>>>> 8cee5fb363a9b19f69ef26c7fd9fe8aa2a6df69f
 
 // Configuración de rutas
 const mainController = new MainController();
 app.get('/', mainController.getIndex.bind(mainController));
 app.get('/index', mainController.getIndex.bind(mainController));
 
-<<<<<<< HEAD
 app.use(cors({
   origin: '*',
-  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
 }));
 
 
 // Iniciar el servidor
 app.listen(port, () => {
-  console.log(`La aplicación está escuchando en http://localhost:${port}`);
+  console.log(`La aplicación está corriendo en http://localhost:${port}`);
 });
+
 
 // Ruta de inicio
 app.get('/', (req, res) => {
@@ -79,15 +65,6 @@ app.get('/', (req, res) => {
 // Rutas para gestionar los datos de las tablas
 const client = require('./routes/clientRoute');
 app.use('/clients', client);
-=======
-const authController = new AuthController();
-// /login
-app.get('/login', authController.getLogin.bind(authController));
-app.post('/login', authController.getRegister.bind(authController));
-// register
-app.post('/register', authController.getRegister.bind(authController));
-app.get('/register', authController.getRegister.bind(authController));
->>>>>>> 8cee5fb363a9b19f69ef26c7fd9fe8aa2a6df69f
 
 // Middleware para manejar errores
 app.use((err, req, res, next) => {
