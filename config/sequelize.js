9633eafcf7424484ff7522000b8acb0a5e6497d6
eagl2002 <41276576+eagl2002@users.noreply.github.com>
2024-03-03 18:49:23 +0000

    Log In Redesign

--- config/sequelize.js
+++ config/sequelize.js
@@ -2,7 +2,7 @@
 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('iguanarepublicdb', 'root', '$Fabricio160603', {
+const sequelize = new Sequelize('iguanarepublicdb', 'root', '1234', {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306,
