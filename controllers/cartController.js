83e873de15497a80fbac63e5ae87e74c6116133a
Fabricio1606 <falvarezcubero16@gmail.com>
2024-04-08 22:47:09 -0600

    Corregcion de errores

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -25,7 +25,10 @@ cartController.showCart = async (req, res) => {
 cartController.getAllCarts = async (req, res) => {
   res.locals.user = req.session.client;
   const user = res.locals.user;
-  res.render("carts", { user: user });
+  res.render("cart", {
+    user: res.locals.user.userClient,
+    admin: res.locals.user.adminUser,
+  });
 };
 
 cartController.addToCart = async (req, res) => {
@@ -33,104 +36,137 @@ cartController.addToCart = async (req, res) => {
   res.locals.user = req.session.client;
   const user = res.locals.user;
 
-  if(user) {
+  if (user) {
     const cart = await Cart.findOne({
       include: Client,
       where: {
-        ClientIdClient : user.idClient,
-        stateCart : 1
-      }
+        ClientIdClient: user.idClient,
+        stateCart: 1,
+      },
     });
 
-    if(cart == null) {
+    if (cart == null) {
       await Cart.create({
         stateCart: 1,
         totalPriceCart: priceProd,
-        ClientIdClient: user.idClient
+        ClientIdClient: user.idClient,
       });
-      
+
       const newCart = await Cart.findOne({
         include: Client,
         where: {
-          ClientIdClient : user.idClient,
-          stateCart : 1
-        }
+          ClientIdClient: user.idClient,
+          stateCart: 1,
+        },
       });
 
       await CartDetail.create({
         quantity: quantityProd,
         unitPrice: priceProd,
         CartIdCart: newCart.idCart,
-        ProductIdProd: idProd
-      })
-      await Product.decrement("stockProd", { by: quantityProd, where: { idProd: idProd } })
-        
-      var total = await CartDetail.findOne({
-        attributes: [[sequelize.fn("SUM", sequelize.literal("quantity * unit_price")), "total"]]
-      }, {
-        where: {
-          CartIdCart: cart.idCart
-        }
-      })
+        ProductIdProd: idProd,
+      });
+      await Product.decrement("stockProd", {
+        by: quantityProd,
+        where: { idProd: idProd },
+      });
 
-      await Cart.update({
-        totalPriceCart: total.dataValues.total
-      }, {
-        where: {
-          idCart: cart.idCart
+      var total = await CartDetail.findOne(
+        {
+          attributes: [
+            [
+              sequelize.fn("SUM", sequelize.literal("quantity * unit_price")),
+              "total",
+            ],
+          ],
+        },
+        {
+          where: {
+            CartIdCart: cart.idCart,
+          },
         }
-      })
+      );
+
+      await Cart.update(
+        {
+          totalPriceCart: total.dataValues.total,
+        },
+        {
+          where: {
+            idCart: cart.idCart,
+          },
+        }
+      );
 
       res.json({
-        result : 1
-      })
-
+        result: 1,
+      });
     } else {
       const detail = await CartDetail.findOne({
         where: {
           CartIdCart: cart.idCart,
-          ProductIdProd: idProd
-        }
+          ProductIdProd: idProd,
+        },
       });
 
-      if(detail == null) {
+      if (detail == null) {
         await CartDetail.create({
           quantity: quantityProd,
           unitPrice: priceProd,
           CartIdCart: cart.idCart,
-          ProductIdProd: idProd
+          ProductIdProd: idProd,
         });
-        await Product.decrement("stockProd", { by: quantityProd, where: { idProd: idProd } })
+        await Product.decrement("stockProd", {
+          by: quantityProd,
+          where: { idProd: idProd },
+        });
       } else {
-        await detail.increment("quantity", { by: quantityProd, where: { CartIdCart: cart.idCart } });
-        await Product.decrement("stockProd", { by: quantityProd, where: { idProd: idProd } })
+        await detail.increment("quantity", {
+          by: quantityProd,
+          where: { CartIdCart: cart.idCart },
+        });
+        await Product.decrement("stockProd", {
+          by: quantityProd,
+          where: { idProd: idProd },
+        });
       }
-        
-      var total = await CartDetail.findOne({
-        attributes: [[sequelize.fn("SUM", sequelize.literal("quantity * unit_price")), "total"]]
-      }, {
-        where: {
-          CartIdCart: cart.idCart
-        }
-      })
 
-      await Cart.update({
-        totalPriceCart: total.dataValues.total
-      }, {
-        where: {
-          idCart: cart.idCart
+      var total = await CartDetail.findOne(
+        {
+          attributes: [
+            [
+              sequelize.fn("SUM", sequelize.literal("quantity * unit_price")),
+              "total",
+            ],
+          ],
+        },
+        {
+          where: {
+            CartIdCart: cart.idCart,
+          },
         }
-      })
+      );
+
+      await Cart.update(
+        {
+          totalPriceCart: total.dataValues.total,
+        },
+        {
+          where: {
+            idCart: cart.idCart,
+          },
+        }
+      );
 
       res.json({
-        result : 1
-      })
+        result: 1,
+      });
     }
   } else {
     res.json({
-      result : 0
-    })
+      result: 0,
+    });
   }
-}
+};
 
 module.exports = cartController;
