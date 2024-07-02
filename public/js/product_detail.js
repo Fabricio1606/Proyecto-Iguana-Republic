1ae17078d265e4276386809d3c7850e6af30375c
Jaible <105011906+Jaible@users.noreply.github.com>
2024-07-01 21:07:29 -0600

    Arreglo de bugs y cambios menores

--- public/js/product_detail.js
+++ public/js/product_detail.js
@@ -119,19 +119,6 @@ var buyBtn = document.querySelector(".buy")
 //     })
 // })
 
-document.addEventListener('mousemove', move);
-function move(e){
-  this.querySelectorAll('.move').forEach(layer =>{
-    const speed = layer.getAttribute('data-speed')
-
-    const x = (window.innerWidth - e.pageX*speed)/120
-    const y = (window.innerWidth - e.pageY*speed)/120
-
-    layer.style.transform = `translateX(${x}px) translateY(${y}px)`
-
-  })
-}
-
 gsap.from('.nav__logo', {opacity: 0, duration: 1, delay: 1, y:10})
 gsap.from('.nav__list .nav__item', {opacity: 0, duration: 1, delay: 1.2, y:30, stagger: 0.2})
 
